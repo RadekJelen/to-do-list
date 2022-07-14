8c63bf9023ca8ae257908805d396f90237268da6
Radosław Jeleń <jelu140@gmail.com>
2022-07-14 14:46:20 +0200

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-{   
+{
     const tasks = [
         {
-            content: "test", 
-            done: false, 
+            content: "test",
+            done: false,
         },
         {
-            content: "test", 
-            done: true, 
+            content: "test",
+            done: true,
         },
     ];
 
-    const render = () => { 
+    const render = () => {
         let htmlString = "";
 
-        for (const task of tasks) { 
-            
+        for (const task of tasks) {
+
             htmlString += ` 
             <li
             class="list__item${task.done ? " list__item--done" : ""}"
@@ -24,12 +24,36 @@
             `;
         }
 
-        document.querySelector(".js-tasks").innerHTML = htmlString; 
+        document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
-    const init = () => {
-        render();
-    }
+        const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
 
-    init();
+            render();
+        };
+
+        const onFormSubmit = (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim(); 
+
+            if (newTaskContent === "") { 
+                return;
+            };
+
+            addNewTask(newTaskContent);
+        };
+
+        const init = () => {
+            render();
+
+            const form = document.querySelector(".js-form");
+
+            form.addEventListener("submit", onFormSubmit);
+        };
+
+        init();
 }
